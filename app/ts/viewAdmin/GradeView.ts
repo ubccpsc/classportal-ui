309969e24d22e863420ee77dbb1f5e56949565c1
Reid Holmes (MBA) <rtholmes@gmail.com>
2017-10-12 19:42:03 -0700

    improve grade logging in UI

--- app/ts/viewAdmin/GradeView.ts
+++ app/ts/viewAdmin/GradeView.ts
@@ -28,31 +28,33 @@ export class GradeView {
 
             var table = '<table style="width: 100%"><tr>';
 
+            if (typeof data.response.students !== 'undefined') {
+                for (let student of data.response.students) {
+                    if (headers === null) {
+                        table += '<th style="text-align:left;">Student</th>';
+                        for (let deliv of student.deliverables) {
+                            if (typeof deliv.final !== 'undefined') {
+                                table += '<th>' + deliv.id + '</th>'
+                            }
+                        }
+                        table += '</tr>';
+                        headers = true;
+                    }
 
-            for (let student of data.students) {
-                if (headers === null) {
-                    table += '<th style="text-align:left;">Student</th>';
+                    table += '<tr>';
+                    table += '<td>' + student.id + '</td>';
                     for (let deliv of student.deliverables) {
                         if (typeof deliv.final !== 'undefined') {
-                            table += '<th>' + deliv.id + '</th>'
+                            table += '<td style="text-align: center;">' + deliv.final + '</td>'
                         }
                     }
                     table += '</tr>';
-                    headers = true;
                 }
-
-                table += '<tr>';
-                table += '<td>' + student.id + '</td>';
-                for (let deliv of student.deliverables) {
-                    if (typeof deliv.final !== 'undefined') {
-                        table += '<td style="text-align: center;">' + deliv.final + '</td>'
-                    }
-                }
-                table += '</tr>';
+                table += '</table>';
+                gradeList.innerHTML = table;
+            } else {
+                gradeList.innerHTML = '<h2 style="text-align: center;">No grades returned.</h2>';
             }
-            table += '</table>';
-
-            gradeList.innerHTML = table;
         } else {
             console.log('GradeView::render - element is null');
         }
