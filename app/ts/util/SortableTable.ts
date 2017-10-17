6f4efd31b6571d55d84c54f8dca39701f31ab733
Reid Holmes (MBA) <rtholmes@gmail.com>
2017-10-17 11:24:11 -0700

    add download scaffolding to the table

--- app/ts/util/SortableTable.ts
+++ app/ts/util/SortableTable.ts
@@ -137,8 +137,11 @@ export class SortableTable {
 
     private endTable() {
         let tableSuffix = '</table>';
+
+
         return tableSuffix;
     }
+
 
     private generateRow(cols: any[], isOdd: boolean) {
         let row = '';
@@ -208,4 +211,48 @@ export class SortableTable {
         });
     }
 
+    // not used yet
+    // code from: https://www.codexworld.com/export-html-table-data-to-csv-using-javascript/
+    private downloadCSV(csv: string, filename: string) {
+        var csvFile;
+        var downloadLink;
+
+        // CSV file
+        csvFile = new Blob([csv], {type: "text/csv"});
+
+        // Download link
+        downloadLink = document.createElement("a");
+
+        // File name
+        downloadLink.download = filename;
+
+        // Create a link to the file
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+
+        // Hide download link
+        downloadLink.style.display = "none";
+
+        // Add the link to DOM
+        document.body.appendChild(downloadLink);
+
+        // Click download link
+        downloadLink.click();
+    }
+
+    private exportTableToCSV(filename: string) {
+        var csv = [];
+        var rows = document.querySelectorAll("table tr");
+
+        for (var i = 0; i < rows.length; i++) {
+            var row = [], cols = rows[i].querySelectorAll("td, th");
+
+            for (var j = 0; j < cols.length; j++)
+                row.push((<HTMLTableCellElement>cols[j]).innerText);
+
+            csv.push(row.join(","));
+        }
+
+        // Download CSV file
+        this.downloadCSV(csv.join("\n"), filename);
+    }
 }
