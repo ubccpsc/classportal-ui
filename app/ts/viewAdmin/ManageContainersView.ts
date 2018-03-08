4adf63e17b57b41b0708e6a0b9d6b4bf84996fda
Andrew Stec <astec@my.bcit.ca>
2018-03-08 15:34:41 -0800

    Debugged some issues

--- app/ts/viewAdmin/ManageContainersView.ts
+++ app/ts/viewAdmin/ManageContainersView.ts
@@ -2,7 +2,7 @@ import {UI} from "../util/UI";
 import {AdminController} from "../controllers/AdminController";
 import {Deliverable, DeliverablePayload, BuildContainerPayload, BuildContainerContainer, 
         DestroyContainerPayload, DestroyContainerContainer, IsContainerBuiltContainer, 
-        IsContainerBuiltPayload} from "../Models";
+        IsContainerBuiltPayload, DeliverableContainer} from "../Models";
 import {Network} from "../util/Network";
 import {OnsModalElement} from "onsenui";
 import {App} from "../App";
@@ -15,6 +15,7 @@ const DOCKER_BUILD_LOGS_CONTAINER = '#adminManageContainers__docker-build-logs-c
 const DOCKER_DESTROY_LOGS_CONTAINER = '#adminManageContainers__docker-destroy-logs-container';
 const NO_DESTROY_HISTORY_CONTAINER = '#adminManageContainers__docker-no-destroy-history-section';
 const NO_BUILD_HISTORY_CONTAINER = '#adminManageContainers__docker-no-build-history-section';
+const CONTAINER_PAGE_ONS_BACK_BUTTON = '#adminManageContainers__back-button';
 
 declare var myApp: App;
 
@@ -51,10 +52,11 @@ export class ManageContainersView {
                         // Cannot click destroy if the container does not exist and updates build status
                         if (isContainerBuiltContainer.response === false) {
                             destroyButton.setAttribute('disabled', 'true');
-                            containerStatus.innerHTML = 'Container Not Built';
+                            containerStatus.innerHTML = 'Container Not Ready';
                         } else {
-                            containerStatus.innerHTML = 'Container Built';
+                            containerStatus.innerHTML = 'Container Ready';
                             buildButton.setAttribute('disabled', 'true');
+                            destroyButton.removeAttribute('disabled');
                             destroyButton.addEventListener('click', () => {
                                 that.confirmDestroyContainer();
                             });
@@ -65,13 +67,14 @@ export class ManageContainersView {
 
     }
 
-    private initBuildButton() {
+    private initBuildButton(isContainerBuilt: boolean) {
         console.log('ManageContainersView::toggleBuildButton(..) - start');
         let that = this;
         let buildButton = document.querySelector(BUILD_CONTAINER_BUTTON) as HTMLElement;
-        if (this.deliverable.buildingContainer) {
+        if (this.deliverable.buildingContainer || isContainerBuilt) {
             buildButton.setAttribute('disabled', 'true');
         } else {
+            buildButton.removeAttribute('disabled');
             buildButton.addEventListener('click', () => {
                 that.confirmBuildContainer();
             });
@@ -86,17 +89,18 @@ export class ManageContainersView {
 
         // Clear Build History by Default for Re-init and then Render
         destroyLogsContainer.innerHTML = '';
+        noDestroyHistoryContainer.innerHTML = '';
 
         let destroyLogCard: string;
 
         // If container is not built and destroy logs exist, then it has been successfully destroyed.
         if (!isContainerBuilt && stdLogDestroy !== '') {
             destroyLogCard = '<ons-card id="adminManageContainers__docker-logs-destroy" style="background-color: #3A70B8; color: #fff">' + 
-                                '<h5>SUCCESSFULLY DESTROYED DOCKER CONTAINER:</h5><p><p><pre>' + stdLogDestroy || '' +  '</pre>' +
+                                '<div class="title">SUCCESSFULLY DESTROYED DOCKER CONTAINER:</div><p><p><pre>' + stdLogDestroy || '' +  '</pre>' +
                              '</ons-card>';
         } else {
             destroyLogCard = '<ons-card id="adminManageContainers__docker-logs-destroy" style="background-color: #B8513A; color: #fff">' + 
-                                '<h5>FAILED TO DESTROY DOCKER CONTAINER:</h5><p><p><pre>' + stdLogDestroy || '' +  '</pre>' +
+                                '<div class="title">FAILED TO DESTROY DOCKER CONTAINER:</div><p><p><pre>' + stdLogDestroy || '' +  '</pre>' +
                              '</ons-card>';
         }
 
@@ -109,6 +113,8 @@ export class ManageContainersView {
         if (stdLogDestroy === '') {
             let noHistoryHtml = UI.ons.createElement('<span>No destroy history to display.</span>')
             noDestroyHistoryContainer.appendChild(noHistoryHtml);
+        } else {
+            noDestroyHistoryContainer.innerHTML = '';
         }
 
     }
@@ -122,8 +128,8 @@ export class ManageContainersView {
         let noBuildHistoryContainer = document.querySelector(NO_BUILD_HISTORY_CONTAINER) as HTMLElement;
 
         // Clear Build History by Default for Re-init and then Render
+        buildLogsContainer.innerHTML = '';
         noBuildHistoryContainer.innerHTML = '';
-
         let buildLogCard: string;
 
         // NOTE ON COMPLEX LOGIC: 
@@ -134,11 +140,11 @@ export class ManageContainersView {
 
         if (isContainerBuilt || (!isContainerBuilt && stdLogBuild !== '' && stdLogDestroy !== '')) {
             buildLogCard = '<ons-card id="adminManageContainers__docker-logs-build" style="background-color: #3A70B8; color: #fff">' + 
-                                '<h5>BUILD SUCCESS LOGS:</h5><p><p><pre>' + stdLogBuild || '' + '</pre>' + 
+                                '<div class="title">BUILD SUCCESS LOGS:</div><p><p><pre>' + stdLogBuild || '' + '</pre>' + 
                             '</ons-card>';
         } else {
             buildLogCard = '<ons-card id="adminManageContainers__docker-logs-build" style="background-color: #B8513A; color: #fff">' + 
-                                '<h5>BUILD FAILED LOGS:</h5><p><p><pre>' + stdLogBuild || '' +  '</pre>' +
+                                '<div class="title">BUILD FAILED LOGS:</div><p><p><pre>' + stdLogBuild || '' +  '</pre>' +
                            '</ons-card>';
         }
 
@@ -155,11 +161,19 @@ export class ManageContainersView {
         }
     }
 
+    private clearLogContainers() {
+        let buildLogsContainer = document.querySelector(DOCKER_BUILD_LOGS_CONTAINER) as HTMLElement;
+        let destroyLogsContainer = document.querySelector(DOCKER_DESTROY_LOGS_CONTAINER) as HTMLElement;
+        buildLogsContainer.innerHTML = '';
+        destroyLogsContainer.innerHTML = '';
+    }
+
     private buildContainer() {
         console.log('ManageContainersView::buildContainer(..) - start');
         let that = this;
         let url = myApp.backendURL + myApp.currentCourseId + '/admin/buildContainer';
         let payload: BuildContainerPayload = {deliverableName: that.deliverable.name};
+
 
         Network.httpPut(url, payload)
             .then((data: any) => {
@@ -168,8 +182,53 @@ export class ManageContainersView {
                         console.log('ManageContainersView::buildContainer(..) Network response: ', buildContainerContainer);
                         // Surprisingly nice UI message:
                         UI.notification(buildContainerContainer.response);
+                        that.clearLogContainers();
+                        that.deliverable.buildingContainer = true;
+                        that.refreshDelivInitTimer();
                     });
             });
+    }
+
+
+    // Set a timer because the container build process is so long that it is easier to refresh the view
+    // if building to determine if progress has been made. Up to 30 minutes build time...
+    // refresh deliv manually, as we are not using socket io yet.
+    // Can be run for long container build task.
+    private refreshDelivInitTimer() {
+        let that = this;
+        let backButton = document.querySelector(CONTAINER_PAGE_ONS_BACK_BUTTON) as HTMLElement;
+        let timers = [];
+        for (let i = 0; i < 30; i++) {
+            let timer = setTimeout(function() {
+               if (that.deliverable.buildingContainer && this === 'undefined') {
+                   console.log('ManageContainersView::initView() Time triggered page re-init for building container refresh');
+                   that.refreshDeliv()
+                       .then(() => {
+                            that.initView();
+                       });
+               }
+            }, i * 30000);
+            timers.push(timer);
+        }
+        backButton.addEventListener('click', () => {
+            timers.map((timer) => {
+                clearTimeout(timer);
+            });
+        });
+    }
+
+    private refreshDeliv(): Promise<Deliverable> {
+        let url = myApp.backendURL + myApp.currentCourseId + '/deliverables';
+        let that = this;
+        return Network.httpGet(url)
+           .then((delivContainer: DeliverableContainer) => {
+               delivContainer.response.map((deliv) => {
+                   if (deliv.name === that.deliverable.name) {
+                       that.deliverable = deliv;
+                   }
+               });
+               return that.deliverable;
+           });
     }
 
     private destroyContainer() {
@@ -185,7 +244,7 @@ export class ManageContainersView {
                         console.log('ManageContainersView::destroyContainer(..) Netowrk response: ', destroyContainerContainer);
                         that.deliverable = destroyContainerContainer.response;
                         that.initView();
-                        UI.notification('Container successfully desotroyed');
+                        UI.notification('Container successfully destroyed.');
                     });
             });
 
@@ -235,15 +294,16 @@ export class ManageContainersView {
     }
 
     private initView() {
+        let that = this;
         UI.showModal();
         this.updateContainerStatus()
             .then((isContainerBuiltContainer: IsContainerBuiltContainer) => {
                 let isContainerBuilt = isContainerBuiltContainer.response;
                 this.updateDelivInputs();
-                this.initBuildButton();
+                this.initBuildButton(isContainerBuilt);
                 this.initBuildLogs(isContainerBuilt);
                 this.initDestroyLogs(isContainerBuilt);
+                UI.hideModal();
             });
-        UI.hideModal();
     }
 }
